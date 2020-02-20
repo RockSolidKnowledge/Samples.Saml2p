f3cb33ff619cbd58072fcda3e973a85c12751d68
Scott Brady <scott@scottbrady91.com>
2020-02-20 10:50:11 +0000

    Removed old demo license

--- spWithIdpInitiated/Startup.cs
+++ spWithIdpInitiated/Startup.cs
@@ -53,8 +53,8 @@ namespace sp
 
             services.AddAuthentication()
                 .AddSaml2p("saml2p", options => {
-                    options.Licensee = "Demo";
-                    options.LicenseKey = "eyJTb2xkRm9yIjowLjAsIktleVByZXNldCI6NiwiU2F2ZUtleSI6ZmFsc2UsIkxlZ2FjeUtleSI6ZmFsc2UsImF1dGgiOiJERU1PIiwiZXhwIjoiMjAxOS0wOS0wOVQwMDowMDowMCIsImlhdCI6IjIwMTktMDgtMDlUMDg6MDY6NDEuMDAwNjU4MSIsIm9yZyI6IkRFTU8iLCJhdWQiOjJ9.FzcifjW8YUPu6icRjrMn1NYv/KJt7ks/m/TrUTZJO1yPDns9v1nNM4++jHObmazmmpzepBQrxJYZpIwEMCovVAubEptH9jIAEr+Y6/tgMQurql9UocrfgPr15J5520XqLB7ExOwCsGI2bm3f0pY/bx2H5Pesg92Ii11S/ELHvnQ3JdyTnaLqpMbjXA0Wcc2jZS/VZ2cpNTItXzA4SxYMaSF1qh3N7ZlkTk+zv8RKHYimdKgKr+bVEc9SS/VEr2NpJcq/dyDl/bwhRg2N9by8h+SdZhyX0ELA50RB8vIUqwG281Upye4cfmY0jlsAyrcU2IvEbokq3Q/IpnuVIFh1Q7Ymh0dP1T327e5n+YvqvZQCZdF2nnAmwQ3P85Kg9TFxkXbbAEEDQ+5yTyllOqsRVVxZ8JCuyQY1Rub6DGN+gKoC/BLQ0+HbdPxcXLr3OX/wj/7/XuQzoLU8bOLC/oNj2tITI10U3W2ycx/F38+nosdLeTVMBfKuJ+rGvFbRYmpuXdBRwxClgYEfOa5gq9aGBvmE2fFHqwkSOcWK2tVLwcN0OqOkkX5D7sn2F5RbXhhentFvRGqAAP8BKEA1214SWzLrAdCxVLOcOk72B8HcRNPPJjnH32CeGwH/w+vGsatvlrz1zpRZdqcjKjrFAxIO4YDvNQ67unZKAR+o7i8CI+s=";
+                    options.Licensee = "";
+                    options.LicenseKey = "";
 
                     options.IdentityProviderOptions = new IdpOptions
                     {
