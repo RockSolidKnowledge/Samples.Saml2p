c747640fbf7d922450b3a6d7981f1c365600c381
Sean Farrow <sean.farrow@rocksolidknowledge.com>
2023-12-14 11:34:47 +0000

    Update the Rsk.Saml.* packages to version 8.0.0 except any projects that work with IdentityServer version 4.

--- OpenIddict/OpenIddictIdP/Startup.cs
+++ OpenIddict/OpenIddictIdP/Startup.cs
@@ -12,11 +12,8 @@ using Rsk.Saml.Configuration;
 using Rsk.Saml.OpenIddict.AspNetCore.Identity.Configuration.DependencyInjection;
 using Rsk.Saml.OpenIddict.Configuration.DependencyInjection;
 using openiddictidp.Data;
-<<<<<<< HEAD
 using Rsk.Saml.Samples;
-=======
 using Rsk.Saml.OpenIddict.EntityFrameworkCore.Configuration.DependencyInjection;
->>>>>>> d677df3 (Ensure all projects target .Net 6.0 and that all packages are upgraded to the latest minor versions that work with .Net 6.)
 using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace openiddictidp;
